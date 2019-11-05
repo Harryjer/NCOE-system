32c997c87dbdb60c8e9f768f48f28c32408aee8d
Harryjer <maroonsugar95@gmail.com>
2019-11-05 15:32:14 +0530

    second component(half)
    result submission (A/L)

--- PersonalDetails-form/src/app/app.module.ts
+++ PersonalDetails-form/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonalDetailFormComponent } from './personal-detail-form/personal-detail-form.component';
 import { ResultdetailsComponent } from './resultdetails/resultdetails.component';
+import { Route, Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+{ path: 'personal-detail-form',component: PersonalDetailFormComponent },
+{ path: 'resultdetails', component: ResultdetailsComponent}
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +22,8 @@ import { ResultdetailsComponent } from './resultdetails/resultdetails.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]

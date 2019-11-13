ff4590a2313ed59755b121abffd50587c40eb940
Harryjer <maroonsugar95@gmail.com>
2019-11-13 15:35:11 +0530

    2.1
    bug fixes

--- PersonalDetails-form/src/app/resultdetails/resultdetails.component.ts
+++ PersonalDetails-form/src/app/resultdetails/resultdetails.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-resultdetails',
   templateUrl: './resultdetails.component.html',
   styleUrls: ['./resultdetails.component.css']
 })
+
 export class ResultdetailsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    
   }
   
 }
+
+

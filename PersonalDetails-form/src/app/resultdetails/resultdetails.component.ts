2617c83ba9b0fcf0ee5a518298ab08ab1e786563
Harryjer <maroonsugar95@gmail.com>
2019-11-14 11:17:05 +0530

    2.2
    included the results submission tables

--- PersonalDetails-form/src/app/resultdetails/resultdetails.component.ts
+++ PersonalDetails-form/src/app/resultdetails/resultdetails.component.ts
@@ -1,21 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-resultdetails',
   templateUrl: './resultdetails.component.html',
   styleUrls: ['./resultdetails.component.css']
 })
-
 export class ResultdetailsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-    
   }
   
 }
+
 
 
